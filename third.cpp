140be939be16b5e65e42c2090edd5acb85e134d6
Aamir Khurshid <108178409+Aamir-Khurshid@users.noreply.github.com>
2022-12-28 20:25:20 +0530

    Add files via upload

---
+++ third.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 3.0/4;                // int/int will be int
+    double b = 3.0/4;            //  double/int will be double
+    float c = 3.0/4;
+    cout << a << endl << b << endl << c;
+
+}
